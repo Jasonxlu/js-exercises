0a1e5da69376be90992389b3277a32c34b4cf26e
Jason <jasonchicago@att.net>
2022-07-25 12:49:18 -0500

    Practicing DOM Manipulation

---
+++ practice.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-contentddddd!';
+container.appendChild(content);
+
+const redtext = document.createElement('p');
+redtext.classList.add('content');
+redtext.textContent = "Hey I'm red!";
+redtext.style.color = 'red';
+container.appendChild(redtext);
+
+const bluehead = document.createElement('h3')
+bluehead.textContent = "I'm a blue h3!";
+bluehead.style.color = 'blue';
+container.appendChild(bluehead);
+
+const pinkdiv = document.createElement('div');
+pinkdiv.style['border-style'] = "solid";
+pinkdiv.style['background-color'] = "pink";
+const headin = document.createElement('h1');
+headin.textContent = "I'm in a div"
+const pin = document.createElement('p');
+pin.textContent = "ME TOO!"
+pinkdiv.appendChild(headin);
+pinkdiv.appendChild(pin);
+container.appendChild(pinkdiv);
